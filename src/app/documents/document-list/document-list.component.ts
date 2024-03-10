fb908734a300e49cf3369e3a511951fb79050e01
Yoli-Buendia <bue17003@byui.edu>
2024-03-10 00:10:15 -0600

    Create and Subscribe to a Subject to Update Documents List

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,25 +1,34 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit, OnDestroy } from '@angular/core';
 import { Document } from '../document.model';
 import { DocumentsService } from '../documents.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-document-list',
   templateUrl: './document-list.component.html',
   styleUrl: './document-list.component.css'
 })
-export class DocumentListComponent {
+export class DocumentListComponent implements OnInit, OnDestroy {
 
   documents: Document[] =[];
+  subscription: Subscription;
+
 
   constructor(private documentService: DocumentsService) {}
 
   ngOnInit(){
     this.documents = this.documentService.getDocuments();
-    this.documentService.documentChangedEvent
+    this.subscription = this.documentService.documentListChangedEvent
       .subscribe(
-        (documents: Document[]) => {
-          this.documents = documents;
-        }
+        (documentsList: Document[]) => {
+          this.documents = documentsList;
+        },
       );
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
